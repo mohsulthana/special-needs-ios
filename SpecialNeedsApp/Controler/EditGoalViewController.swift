77903a5260534c119d86528b4c9c82011264ba2c
mohammadsulthan <muhammadsultana@outlook.com>
2023-06-20 00:15:05 +0700

    feat: fixed on selecting date

---
+++ SpecialNeedsApp/Controler/EditGoalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditGoalViewController.swift
+//  SpecialNeedsApp
+//
+//  Created by Mohammad Sulthan on 19/06/23.
+//  Copyright © 2023 Gustavo Ortega. All rights reserved.
+//
+
+import UIKit
+
+class EditGoalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
